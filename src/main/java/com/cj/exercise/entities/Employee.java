016e07b91ab3baf872046c671179f537dc79aa98
mitzyvalenciaordonez <mitzyvo@gmail.com>
2024-01-28 12:20:34 -0500

    Equals added

--- src/main/java/com/cj/exercise/entities/Employee.java
+++ src/main/java/com/cj/exercise/entities/Employee.java
@@ -2,5 +2,17 @@ package com.cj.exercise.entities;
 
 public record Employee(Integer id, String name, Float monthlyAmount, Boolean active) {
 
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Employee employee)) {
+            return false;
+        }
+        return employee.id.equals(this.id) && employee.name.equals(this.name);
+    }
 }
